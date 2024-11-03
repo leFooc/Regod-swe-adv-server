7089326a2c898441e32df8efb3a92a910b7c4b3d
tdhuy1964 <tdhuy1964@gmail.com>
2024-11-03 10:03:03 +0700

    add package structure

---
+++ src/main/java/com/regod/app/models/bills/Bill.java
@@ -0,0 +1,13 @@
+﻿package com.regod.app.models.bills;
+import lombok.Getter;
+import lombok.Setter;
+public class Bill {
+    @Getter @Setter
+    private String name;
+    @Getter @Setter
+    private String departmentName;
+    @Getter @Setter
+    private String deadlineDate;
+    //@Getter
+    //private
+}
---
+++ src/main/java/com/regod/app/repositories/BillRepository.java
@@ -0,0 +1,15 @@
+﻿package com.regod.app.repositories;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BillRepository {
+    public BillRepository(){
+
+    }
+
+    public void GetBillByID(String ID){
+        return
+    }
+}
