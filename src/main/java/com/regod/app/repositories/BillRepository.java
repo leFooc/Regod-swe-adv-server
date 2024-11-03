7c2824c4f4ff591b84395577e6e3aa43ef61eb20
Le Phuc <nglephuc03@gmail.com>
2024-11-03 10:08:39 +0700

    test rule

--- src/main/java/com/regod/app/repositories/BillRepository.java
+++ src/main/java/com/regod/app/repositories/BillRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BillRepository {
     public BillRepository(){
-
+        Integer a = 1;
     }
 
     public void GetBillByID(String ID){
