7281e581d2247edf7a48deb6665be90bb1387fb4
Le Phuc <nglephuc03@gmail.com>
2024-11-09 10:44:01 +0700

    fix cors

--- src/main/java/com/regod/app/AppApplication.java
+++ src/main/java/com/regod/app/AppApplication.java
@@ -18,7 +18,10 @@ public class AppApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+				;
 
 			}
 		};
